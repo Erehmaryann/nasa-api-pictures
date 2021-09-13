ce9697258bba8e3e479a11af068d90389e7a62c0
Ereh Maryann <edwardmaryann09@gmail.com>
2021-09-12 17:39:28 -0700

    finished creating all the dom elements

--- script.js
+++ script.js
@@ -14,13 +14,57 @@ const apiURL = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${co
 
 let resultsArray = [];
 
+// update the Dom
+function updateDOM() {
+    // Loop through the resultsArray
+    resultsArray.forEach(result => {
+        // Create the Card Container
+        const card = document.createElement('div');
+        card.classList.add('card');
+        // Create the Link to the image
+        const link = document.createElement('a');
+        link.setAttribute('href', result.hdurl);
+        link.setAttribute('target', '_blank');
+        link.setAttribute("title", "View Full Image");
+        // Create the Image
+        const image = document.createElement('img');
+        image.setAttribute('src', result.url);
+        image.setAttribute('alt', result.title);
+        image.setAttribute('loading', "lazy");
+        image.classList.add('card-img-top');
+        // Create the Card Body
+        const cardBody = document.createElement('div');
+        cardBody.classList.add('card-body');
+        // Create the Card Title
+        const cardTitle = document.createElement('h5');
+        cardTitle.classList.add('card-title');
+        cardTitle.textContent = result.title;
+        // Create the Save Text
+        const saveText = document.createElement('p');
+        saveText.classList.add('clickable');
+        saveText.textContent = "Add To Favorites";
+        // Create the Card Text
+        const cardText = document.createElement('p');
+        cardText.textContent = result.explanation;
+        // Create the Footer
+        const footer = document.createElement('small');
+        footer.classList.add('text-muted');
+        // Create the Date
+        const date = document.createElement('strong');
+        date.textContent = result.date;
+        // Create CopyRight
+        const copyright = document.createElement('span');
+        copyright.textContent = `${results.copyright}`;
+    });
+}
+
 // Get 10 random images from NASA API
 async function getNasaImages() {
     try {
         const response = await fetch(apiURL);
         resultsArray = await response.json();
         console.log(resultsArray);
-        return resultsArray;
+        updateDOM();
     } catch (error) {
         // Catch any errors
         console.log(error);
