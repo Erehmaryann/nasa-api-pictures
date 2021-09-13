1c3aa54d471428cd71c93a7ad5a1ffa2e6be542d
Ereh Maryann <edwardmaryann09@gmail.com>
2021-09-12 17:13:16 -0700

    setup the NASA apod api

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+
+// NASA API
+const count = 10;
+const apiKey = "hAKOSdL1nwBeS7zom2dzkFPxgxwZHoRX2H1tTZto";
+const apiURL = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
+
+let resultsArray = [];
+
+// Get 10 random images from NASA API
+async function getNasaImages() {
+    try {
+        const response = await fetch(apiURL);
+        resultsArray = await response.json();
+        console.log(resultsArray);
+        return resultsArray;
+    } catch (error) {
+        // Catch any errors
+        console.log(error);
+    }
+}
+
+getNasaImages();
